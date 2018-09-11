29f0d09a9da28d92508cb7f07bb5a7a5c9887efc
Dhrupal Patel <dhrupal.patel@infostretch.com>
2018-09-11 17:41:42 +0530

    app.js file

---
+++ App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Reusable1} from '@components/Reusable1';
+
+export default class App extends Component {
+  render () {
+    return <Reusable1 />;
+  }
+}
